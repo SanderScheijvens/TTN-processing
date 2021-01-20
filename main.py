ba066dd62a9ff5d612a233e28adf2e42d4d7980c
SanderScheijvens <50071969+SanderScheijvens@users.noreply.github.com>
2021-01-20 17:15:18 +0100

    versie 0.1

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from modules import api, database, python_mysql_dbconfig
 import time
 
 if __name__ == "__main__":
-    while True:
         data_tem, data_hum, data_time, device = api.get_data()
         print(f"temp: {data_tem}\n"
               f"hum: {data_hum}\n"
@@ -10,4 +9,3 @@ if __name__ == "__main__":
         time_db = database.get_time(data_time)
         if time_db != data_time:
             c = database.send_data(data_tem, data_hum, data_time, device)
-        time.sleep(10)
